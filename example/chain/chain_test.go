a48823055babcae05d4fa6276e12fed228cd1220
anqiansong <anqiansong@xiaoheiban.cn>
2021-04-06 11:06:51 +0800

    Add chain_test.go

---
+++ example/chain/chain_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Do func() error
+
+func ExampleChain() {
+	list := []Do{
+		func() error {
+			fmt.Println("1")
+			return nil
+		},
+		func() error {
+			fmt.Println("2")
+			return nil
+		},
+		func() error {
+			fmt.Println("3")
+			return nil
+		},
+	}
+
+	fn := func() error {
+		for _, fn := range list {
+			err := fn()
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	fn()
+}
